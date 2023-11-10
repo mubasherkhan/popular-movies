be9fa82a20e9295a34283164fcc7c7eaffe81652
mubasherkhan_codingpixel <mubasher.khan@codingpixel.com>
2023-11-10 15:18:13 +0500

    Initial Project

---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Mubasher Khan on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
